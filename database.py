6e9cb70785cf2d07dbbf86a6820f98ffaf91cae1
Aline Branco <79863993+alinebw@users.noreply.github.com>
2024-12-10 12:34:13 -0300

    Atualização de app.py e database.py para funcionamento na Lambda

--- database.py
+++ database.py
@@ -1,36 +1,29 @@
 import os
 import pymysql
+import logging
 from pymysql.cursors import DictCursor
 from dotenv import load_dotenv
-import logging
 
-# Configuração de logger monitoramento
-logging.basicConfig(
-    level=logging.INFO,
-    filename='processamento.log',
-    format='%(asctime)s %(levelname)s %(message)s'
-)
-
-def log_event(message, level=logging.INFO):
-    """
-    Registra um evento nos logs do sistema
-    """
-    logging.log(level, message)
-
-# Carregar variáveis de ambiente
 load_dotenv()
 
-# Configurações do banco de dados
+def log_event(mensagem, nivel=logging.INFO):
+    logging.log(nivel, mensagem)
+
+# Configuração de conexão com o banco de dados
 DB_HOST = os.getenv('DB_HOST')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_NAME = os.getenv('DB_NAME')
 DB_PORT = int(os.getenv('DB_PORT', 3306))
 
+# Check variáveis de ambiente
+logging.info(f"DB_HOST: {DB_HOST}")
+logging.info(f"DB_USER: {DB_USER}")
+logging.info(f"DB_NAME: {DB_NAME}")
+logging.info(f"DB_PORT: {DB_PORT}")
+
 def get_db_connection():
-    """
-    Estabelece conexão com o banco de dados
-    """
+    """Estabelece conexão com o banco de dados"""
     try:
         connection = pymysql.connect(
             host=DB_HOST,
@@ -52,12 +45,13 @@ def validate_data(field_name, value, expected_type, max_length=None):
     """
     Valida dados antes da inserção no banco
     """
+    logging.info(f"Validando {field_name}: valor={value}, tipo={type(value)}")
     if value is not None and not isinstance(value, expected_type):
         raise ValueError(f"{field_name} deve ser do tipo {expected_type.__name__}.")
     if max_length and isinstance(value, str) and len(value) > max_length:
         raise ValueError(f"{field_name} excede o tamanho máximo de {max_length} caracteres.")
 
-def insert_checklist(connection, id_checklist, nome_checklist=None):
+def insert_checklist(connection, id_checklist):
     """
     Insere um checklist no banco de dados, se ainda não existir.
     """
@@ -69,15 +63,13 @@ def insert_checklist(connection, id_checklist, nome_checklist=None):
 
             if not exists:
                 sql_insert = """
-                    INSERT INTO checklists (id_checklist, nome_checklist)
-                    VALUES (%s, %s)
+                    INSERT INTO checklists (id_checklist)
+                    VALUES (%s)
                 """
                 # Validação de dados
-                validate_data("id_checklist", id_checklist, int)
-                if nome_checklist is not None:
-                    validate_data("nome_checklist", nome_checklist, str, 100)
+                validate_data("id_checklist", id_checklist, str, max_length=45)
 
-                cursor.execute(sql_insert, (id_checklist, nome_checklist))
+                cursor.execute(sql_insert, (id_checklist,))
                 log_event(f"Checklist {id_checklist} inserido com sucesso.")
             else:
                 log_event(f"Checklist {id_checklist} já existe. Nenhuma inserção realizada.")
@@ -86,8 +78,7 @@ def insert_checklist(connection, id_checklist, nome_checklist=None):
         log_event(f"Erro ao inserir checklist {id_checklist}: {e}", logging.ERROR)
         raise
 
-
-def insert_avaliacao(connection, id_avaliacao, id_checklist, tipo_avaliacao=None):
+def insert_avaliacao(connection, id_avaliacao, id_checklist):
     """
     Insere uma avaliação no banco de dados, se ainda não existir.
     """
@@ -99,17 +90,17 @@ def insert_avaliacao(connection, id_avaliacao, id_checklist, tipo_avaliacao=None
 
             if not exists:
                 sql_insert = """
-                    INSERT INTO avaliacoes (id_avaliacao, id_checklist, tipo_avaliacao, status)
-                    VALUES (%s, %s, %s, %s)
+                    INSERT INTO avaliacoes (id_avaliacao, id_checklist, status)
+                    VALUES (%s, %s, %s)
                 """
                 # Validação de dados
-                validate_data("id_avaliacao", id_avaliacao, int)
-                validate_data("id_checklist", id_checklist, int)
+                validate_data("id_avaliacao", id_avaliacao, str, max_length=45)
+                validate_data("id_checklist", id_checklist, str, max_length=45)
 
-                status = 'Em andamento'  # Definir o status como "Em andamento"
+                status = 'Em andamento'  # Definir o status como "Em andamento" para identificar forms que chegam de avaliação realmente em andamento
 
                 cursor.execute(sql_insert, (
-                    id_avaliacao, id_checklist, tipo_avaliacao, status
+                    id_avaliacao, id_checklist, status
                 ))
                 log_event(f"Avaliação {id_avaliacao} inserida com sucesso.")
             else:
@@ -119,8 +110,7 @@ def insert_avaliacao(connection, id_avaliacao, id_checklist, tipo_avaliacao=None
         log_event(f"Erro ao inserir avaliação {id_avaliacao}: {e}", logging.ERROR)
         raise
 
-
-def insert_entregavel(connection, id_entregavel, id_avaliacao, data_recebimento, id_typeform, nome_respondente, comentario_obrigatorio, comentario_opcional, id_checklist):
+def insert_entregavel(connection, id_entregavel, id_avaliacao, data_recebimento, nome_respondente, comentario_obrigatorio, comentario_opcional, id_checklist):
     """
     Insere um entregável no banco de dados, se ainda não existir.
     """
@@ -132,15 +122,15 @@ def insert_entregavel(connection, id_entregavel, id_avaliacao, data_recebimento,
 
             if not exists:
                 sql_insert = """
-                    INSERT INTO entregaveis (id_entregavel, id_avaliacao, data_recebimento, id_typeform, nome_respondente, comentario_obrigatorio, comentario_opcional, id_checklist)
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                    INSERT INTO entregaveis (id_entregavel, id_avaliacao, data_recebimento, nome_respondente, comentario_obrigatorio, comentario_opcional, id_checklist)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s)
                 """
                 # Validação de dados
-                validate_data("id_entregavel", id_entregavel, str, 255)
-                validate_data("id_avaliacao", id_avaliacao, int)
+                validate_data("id_entregavel", id_entregavel, str, max_length=255)
+                validate_data("id_avaliacao", id_avaliacao, str, max_length=45)
 
                 cursor.execute(sql_insert, (
-                    id_entregavel, id_avaliacao, data_recebimento, id_typeform,
+                    id_entregavel, id_avaliacao, data_recebimento,
                     nome_respondente, comentario_obrigatorio, comentario_opcional, id_checklist
                 ))
                 log_event(f"Entregável {id_entregavel} inserido com sucesso.")
@@ -168,17 +158,21 @@ def ensure_pergunta_exists(connection, id_pergunta, id_avaliacao, field):
                 """
                 texto_pergunta = field.get('title')
                 tipo_pergunta = field.get('type')
-                ordem = field.get('ordem')
+                ordem = field.get('ordem', 0)
                 ref = field.get('ref')
+                
+                # Validação e ajuste de texto_pergunta
+                if not texto_pergunta:  # Trata valores nulos ou vazios
+                    texto_pergunta = "Pergunta não especificada"  # Valor padrão
 
                 # Validação de dados
-                validate_data("id_pergunta", id_pergunta, str, 50)
+                validate_data("id_pergunta", id_pergunta, str, max_length=50)
 
                 cursor.execute(sql_insert, (
                     id_pergunta, id_avaliacao, texto_pergunta,
                     tipo_pergunta, ordem, ref
                 ))
-                log_event(f"Pergunta {id_pergunta} inserida com sucesso.")
+                log_event(f"Pergunta {id_pergunta} inserida com sucesso com texto '{texto_pergunta}'.")
             else:
                 log_event(f"Pergunta {id_pergunta} já existe. Nenhuma inserção realizada.")
 
@@ -201,9 +195,8 @@ def insert_resposta(connection, id_entregavel, id_pergunta, id_avaliacao, valor_
                 valor_resposta, texto_resposta, tipo_resposta, ref
             ))
             log_event(f"Resposta para a pergunta {id_pergunta} inserida com sucesso.")
-        connection.commit()
+        # O commit será feito após todas as operações, não aqui
     except Exception as e:
-        connection.rollback()
         log_event(f"Erro ao inserir resposta para a pergunta {id_pergunta}: {e}", logging.ERROR)
         raise
 
@@ -244,6 +237,7 @@ def log_processamento(connection, id_entregavel, status, mensagem):
             log_event(f"Log registrado para entregável {id_entregavel}: {status} - {mensagem}")
     except Exception as e:
         log_event(f"Erro ao registrar log para entregável {id_entregavel}: {e}", logging.ERROR)
+        raise
 
 #Função para log de eventos gerais
 def log_event(mensagem, nivel=logging.INFO):
